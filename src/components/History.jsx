1c1da6be9d73e16f982a82305e4ff0eb2c532bb1
Shivam-kum-mhta <sk8625228@gmail.com>
2024-03-16 15:22:08 +0530

    save feature added

---
+++ src/components/History.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+
+function History({ Histor}) {
+
+useEffect(() => {
+
+  } ,[Histor])
+
+
+
+return(<div className='disp'  style={{
+  marginTop: "10px",
+  display: "grid",
+
+  gap: "20px",
+  overflow: "auto",
+  height: "95vh",
+  lineThickness: "2px",
+  lineColor: "red",
+}}>
+  {Histor && Histor.map((item) => {
+    return (
+          <div onClick={()=>{ console.log("trigering"); }}
+            style={{ borderRadius: "10px", border: "1px solid purple " }}
+      
+             >
+              <div
+            dangerouslySetInnerHTML={{
+              __html: `<iframe  id="ytplayer" type="text/html" width="100%" height="225" src="https://www.youtube.com/embed/${item.videoId}?enablejsapi=1&controls=0" frameborder="0" modestbranding: "1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>`,
+            }} />
+          </div>)
+         
+})}
+
+</div>)
+
+};
+      
+export default History;
